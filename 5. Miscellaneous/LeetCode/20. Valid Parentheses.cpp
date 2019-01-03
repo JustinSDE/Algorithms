874e476c9e1991ef6300959754f114cc86b2359b
HinderKing <hinderking@126.com>
2019-01-03 13:21:47 -0600

    Update 20. Valid Parentheses.cpp

--- 5. Miscellaneous/LeetCode/20. Valid Parentheses.cpp
+++ 5. Miscellaneous/LeetCode/20. Valid Parentheses.cpp
@@ -24,3 +24,41 @@ class Solution {
         return st.empty();
     }
 };
+
+class Solution2 {
+public:
+    bool isValid(string s) {
+        stack<char> unpaired_parentheses;
+        for(auto ch:s){
+            switch(ch){
+                case '(':
+                case '[':
+                case '{':
+                    unpaired_parentheses.push(ch);
+                    break;
+                case ')':
+                    if(unpaired_parentheses.empty() || unpaired_parentheses.top()!='(')
+                        return false;
+                    else
+                        unpaired_parentheses.pop();
+                    break;
+                case ']':
+                    if(unpaired_parentheses.empty() || unpaired_parentheses.top()!='[')
+                        return false;
+                    else
+                        unpaired_parentheses.pop();
+                    break;
+                case '}':
+                    if(unpaired_parentheses.empty() || unpaired_parentheses.top()!='{')
+                        return false;
+                    else
+                        unpaired_parentheses.pop();
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+        return unpaired_parentheses.empty();
+    }
+};
