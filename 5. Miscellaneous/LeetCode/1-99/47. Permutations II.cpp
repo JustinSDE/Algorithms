d7eb717686d28cbe06dfd954977b370a08d475ce
HinderKing <hinderking@126.com>
2019-01-14 09:06:40 -0600

    Update 47. Permutations II.cpp

--- 5. Miscellaneous/LeetCode/1-99/47. Permutations II.cpp
+++ 5. Miscellaneous/LeetCode/1-99/47. Permutations II.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& input) {
-        sort(input.begin(), input.end());
-	int sz = input.size();
-	vector<vector<int>> res;
-	findpermutation(res, input, 0, sz);
-	return res;
-    }
-    
-  void findpermutation(vector<vector<int>>& res, vector<int> input, int start, int sz)
-{
-	if (start == sz - 1)
-	{
+	void findpermutation(vector<vector<int>> &res, vector<int> input, int start,
+			     int sz) {
+	    if (start == sz - 1) {
 		res.push_back(input);
 		return;
-	}
-	for (int i = start; i < sz; i++)
-	{
-		if (i != start && input[i] == input[start]) continue;
+	    }
+	    for (int i = start; i < sz; i++) {
+		if (i != start && input[i] == input[start])
+		    continue;
 		swap(input[start], input[i]);
 		findpermutation(res, input, start + 1, sz);
+	    }
 	}
-}
+
+	vector<vector<int>> permuteUnique(vector<int> &input) {
+	    sort(input.begin(), input.end());
+	    int sz = input.size();
+	    vector<vector<int>> res;
+	    findpermutation(res, input, 0, sz);
+	    return res;
+	}
 };
